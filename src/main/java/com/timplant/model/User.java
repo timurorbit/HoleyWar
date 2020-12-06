7dc49ad89fd371359af0bd12427efdc074cb3179
Timur <t.shamilov.hollow@gmail.com>
2020-12-06 17:20:59 +0300

    bootstrap realization

--- src/main/java/com/timplant/model/User.java
+++ src/main/java/com/timplant/model/User.java
@@ -26,6 +26,10 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+    public boolean isAdmin() {
+        return roles.contains(Role.ADMIN);
+    }
+
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
